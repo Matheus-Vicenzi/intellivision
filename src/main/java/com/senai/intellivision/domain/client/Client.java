db504d0df904163aa0645ddd59d71afb7bbdb95a
Matheus Vicenzi <matheus.vicenzi2000@gmail.com>
2023-11-20 20:59:43 -0300

    ajustes no cliente

--- src/main/java/com/senai/intellivision/domain/client/Client.java
+++ src/main/java/com/senai/intellivision/domain/client/Client.java
@@ -1,11 +1,13 @@
 package com.senai.intellivision.domain.client;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Client {
     private String macAddress;
     private String clientUsername;
+    private String orgToken;
     private String clientDescription;
-    private String organizationName;
 }
